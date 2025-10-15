29ea25b71f6d36af0095af331af9ae57a4aab2a0
StanNAlexandru <alexnstan@gmail.com>
2025-10-15 15:04:44 +0300

    Create table of users

--- resources/views/components/client-list.blade.php
+++ resources/views/components/client-list.blade.php
@@ -1,7 +1,18 @@
 <div>
-    @forelse($clients as $client)
-        <p>{{ $client->name }}</p>
-    @empty
-        <p>No clients registered yet.</p>
-    @endforelse
+    <table class="bg-slate-300">
+        <thead>
+            <tr>
+                <th class="p-2 border-1">Client name</th>
+                <th class="p-2 border-1">Email</th>
+            </tr>
+            @forelse($clients as $client)
+                <tr>
+                    <td class="p-2 text-center border-1">{{ $client->client_name }}</td>
+                    <td class="p-2 text-center border-1">{{ $client->email }}</td>
+                </tr>
+            @empty
+                <p>No clients registered yet.</p>
+            @endforelse
+        </thead>
+    </table>
 </div>
