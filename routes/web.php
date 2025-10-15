644ed62c765220e3ea9d763261654a824e4d1282
StanNAlexandru <alexnstan@gmail.com>
2025-10-15 17:41:43 +0300

    Create resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,13 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Client;
 
-Route::get('/', function () {
-    $clients = Client::query();
-    $clients->when(request('search'), function($query){
-        $query->where('client_name', 'like', '%' . request('search') . '%')
-              ->orWhere('email', 'like', '%' . request('search') . '%');
-    })->when(request('sort'), function($query){
-        if(request('sort') === 'az'){
-            $query->orderBy('client_name');
-        }
-        else if(request('sort') === 'za'){
-            $query->orderBy('client_name', 'desc');
-        }
-    });
-    return view('home',[
-        'clients' => $clients->get()
-    ]);
-});
+Route::get('', fn() => to_route('client.index'));
 
 Route::get('/clients', function () {
     return view('clients');
 });
+
+Route::resource('client', ClientController::class);
