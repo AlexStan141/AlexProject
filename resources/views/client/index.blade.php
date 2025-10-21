4960643da8b5f158b2b1e668f25e35e679ee0680
StanNAlexandru <alexnstan@gmail.com>
2025-10-21 16:46:48 +0300

    Design changes and policy implementation

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1,39 +1,41 @@
 <x-layout active="Clients">
-    @if(session() -> has('success'))
-        <div>{{session('success')}}</div>
+    @if (session()->has('success'))
+        <div>{{ session('success') }}</div>
     @endif
-    <div class="flex justify-between items-end mb-4">
-        <div>
-            <h1 class="text-3xl">Dashboard</h1>
-            <h2>Here's what's happening in your acount today</h2>
+
+    <div class="flex gap-4 mt-4 items-start">
+        <div class="basis-3/4 flex flex-col gap-4">
+            <div>
+                <h1 class="text-3xl">Dashboard</h1>
+                <h2>Here's what's happening in your acount today</h2>
+            </div>
+            <x-client-list :$clients></x-client-list>
         </div>
-        <div>
+        <div class="basis-1/4 flex flex-col gap-4 mt-7">
             <button class="bg-white py-1 px-2 rounded-md">
                 <a href="{{ route('admin.create') }}">Add Client</a>
             </button>
+            <div class="basis-1/3 bg-white h-[60vh] overflow-scroll rounded-md p-3 mt-4">
+                <h1 class="text-xl font-bold">Filter</h1>
+                <form action="{{ route('admin.clients') }}" method="GET" class="flex flex-col items-start">
+                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search"></x-text-input>
+                    <p class="mt-4">Sort</p>
+                    <div class="flex gap-4">
+                        <input type="radio" name="sort" value="" id="nosort" @checked(!request('sort'))>
+                        <label for="nosort">No sort</label>
+                    </div>
+                    <div class="flex gap-4">
+                        <input type="radio" name="sort" value="az" id="az" @checked(request('sort') === 'az')>
+                        <label for="az">A-Z</label>
+                    </div>
+                    <div class="flex gap-4">
+                        <input type="radio" name="sort" value="za" id="za" @checked(request('sort') === 'za')>
+                        <label for="za">Z-A</label>
+                    </div>
+                    <button type="submit" class="mt-4 bg-blue-500 px-2 py-1 text-white rounded-md">Submit</button>
+                </form>
+            </div>
         </div>
     </div>
-    <div class="flex gap-4">
-        <x-client-list :$clients></x-client-list>
-        <div class="basis-1/3 bg-white h-[60vh] overflow-scroll rounded-md p-3 mt-4">
-            <h1 class="text-xl font-bold">Filter</h1>
-            <form action="{{ route('admin.clients') }}" method="GET" class="flex flex-col items-start">
-                <x-text-input name="search" value="{{ request('search') }}" placeholder="Search"></x-text-input>
-                <p class="mt-4">Sort</p>
-                <div class="flex gap-4">
-                    <input type="radio" name="sort" value="" id="nosort" @checked(!request('sort'))>
-                    <label for="nosort">No sort</label>
-                </div>
-                <div class="flex gap-4">
-                    <input type="radio" name="sort" value="az" id="az" @checked(request('sort') === 'az')>
-                    <label for="az">A-Z</label>
-                </div>
-                <div class="flex gap-4">
-                    <input type="radio" name="sort" value="za" id="za" @checked(request('sort') === 'za')>
-                    <label for="za">Z-A</label>
-                </div>
-                <button type="submit" class="mt-4 bg-blue-500 px-2 py-1 text-white rounded-md">Submit</button>
-            </form>
-        </div>
-    </div>
+
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Middleware\Admin;
 use App\Http\Requests\AdminRequest;
 use App\Http\Requests\ClientRequest;
 use App\Models\Client;
@@ -9,6 +8,7 @@ use App\Models\User;
 use App\Models\Admin as AdminModel;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 Route::get('/', function () {
     return redirect()->route('login');
@@ -23,14 +23,17 @@ Route::get('/dashboard', function () {
 })->name('dashboard');
 
 Route::get('/admin/dashboard', function () {
+    Gate::authorize('view', AdminModel::class);
     return view('admin.show', ['admin' => Auth::user()->admin]);
-})->middleware(['auth', 'verified', 'admin'])->name('admin.dashboard');
+})->name('admin.dashboard');
 
 Route::get('/client/dashboard', function () {
+    Gate::authorize('view', Auth::user()->client);
     return view('client.show', ['client' => Auth::user()->client]);
 })->middleware(['auth', 'verified'])->name('client.dashboard');
 
 Route::get('/admin/clients', function () {
+    Gate::authorize('viewAny', Client::class);
     $clients = Client::with('user');
     $clients->when(request('search'), function ($query) {
         $query->where('full_name', 'like', '%' . request('search') . '%')
@@ -47,18 +50,21 @@ Route::get('/admin/clients', function () {
     return view('client.index', [
         'clients' => $clients->get()
     ]);
-})->middleware(['auth', 'verified', 'admin'])->name('admin.clients');
+})->name('admin.clients');
 
 
 Route::get('/admin/{admin}/edit', function (AdminModel $admin) {
+    Gate::authorize('update', $admin);
     return view('admin.edit', ['admin' => $admin]);
-})->middleware(['auth', 'verified', 'admin'])->name('admin.edit');
+})->name('admin.edit');
 
 Route::get('/client/{client}/edit', function (Client $client) {
+    Gate::authorize('update', $client);
     return view('client.edit', ['client' => $client]);
-})->middleware(['auth', 'verified'])->name('client.edit.user');
+})->name('client.edit.user');
 
-Route::put('/admin/{admin}', function (AdminModel $admin, AdminRequest $request){
+Route::put('/admin/{admin}', function (AdminModel $admin, AdminRequest $request) {
+    Gate::authorize('update', $admin);
     $validated = $request->validated();
     $user = $admin->user;
     $user->update([
@@ -82,6 +88,7 @@ Route::put('/admin/{admin}', function (AdminModel $admin, AdminRequest $request)
 })->name('admin.update');
 
 Route::put('/client/{client}', function (Client $client, ClientRequest $request) {
+    Gate::authorize('update', $client);
     $validated = $request->validated();
     $user = $client->user;
     $user->update([
@@ -104,15 +111,13 @@ Route::put('/client/{client}', function (Client $client, ClientRequest $request)
     return redirect()->route('client.dashboard')->with('success', 'Account updated successfully!');
 })->name('client.update.user');
 
-
-
-
 Route::get('/admin/clients/{client}/edit', function (Client $client) {
+    Gate::authorize('update', $client);
     return view("client.edit", ['client' => $client]);
 })->name("client.edit.admin");
 
 Route::put('/admin/clients/{client}', function (ClientRequest $request, Client $client) {
-
+    Gate::authorize('update', $client);
     $validated = $request->validated();
     $user = $client->user;
     $user->update([
@@ -135,41 +140,36 @@ Route::put('/admin/clients/{client}', function (ClientRequest $request, Client $
     return redirect()->route('admin.clients')->with('success', 'Account updated successfully!');
 })->name('client.update.admin');
 
+Route::get('/admin/clients/create', function () {
+    Gate::authorize('create', Client::class);
+    return view('client.create', [
+        'status' => old('status', null)
+    ]);
+})->name('admin.create');
 
+Route::post('/admin/clients', function (ClientRequest $request) {
+    Gate::authorize('create', Client::class);
+    $validated = $request->validated();
 
+    $user = User::create([
+        'name' => $validated['full_name'],
+        'email' => $validated['user']['email'],
+        'password' => bcrypt($validated['user']['password'])
+    ]);
 
+    Client::create([
+        'full_name' => $validated['full_name'],
+        'phone' => $validated['phone'],
+        'company_name' => $request->input('company_name'),
+        'address' => $request->input('address'),
+        'notes' => $request->input('notes'),
+        'status' => $validated['status'],
+        'user_id' => $user->id
+    ]);
 
+    return redirect()->route('admin.clients')->with('success', 'Account added successfully!');
+})->name('client.store');
 
-Route::middleware(['auth', 'verified', 'admin'])->group(function () {
-    Route::get('/admin/clients/create', function () {
-        return view('client.create', [
-            'status' => old('status', null)
-        ]);
-    })->name('admin.create');
-
-    Route::post('/admin/clients', function (ClientRequest $request) {
-
-        $validated = $request->validated();
-
-        $user = User::create([
-            'name' => $validated['full_name'],
-            'email' => $validated['user']['email'],
-            'password' => bcrypt($validated['user']['password'])
-        ]);
-
-        Client::create([
-            'full_name' => $validated['full_name'],
-            'phone' => $validated['phone'],
-            'company_name' => $request->input('company_name'),
-            'address' => $request->input('address'),
-            'notes' => $request->input('notes'),
-            'status' => $validated['status'],
-            'user_id' => $user->id
-        ]);
-
-        return redirect()->route('admin.clients')->with('success', 'Account added successfully!');
-    })->name('client.store');
-});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
